a89bd390161c83edc4ae69d24ac89035403954ba
Thiago Cechetto <thiagocechetto@Cechettos-MacBook-Pro.local>
2017-09-11 16:29:55 -0300

    Added Kotlin support.

---
+++ Umbrella/src/main/java/com/foo/umbrella/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.foo.umbrella.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.foo.umbrella.R
+
+class MainActivity: AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
