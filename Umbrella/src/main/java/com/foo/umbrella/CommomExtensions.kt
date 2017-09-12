122771fbfbed8a5b4ae145af683d611ca20d5541
Thiago Cechetto <thiagocechetto@Cechettos-MacBook-Pro.local>
2017-09-12 18:37:22 -0300

    Created Hourly list.

--- Umbrella/src/main/java/com/foo/umbrella/CommomExtensions.kt
+++ Umbrella/src/main/java/com/foo/umbrella/CommomExtensions.kt
@@ -3,7 +3,12 @@ package com.foo.umbrella
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
+import org.threeten.bp.LocalDateTime
+import org.threeten.bp.format.DateTimeFormatter
 
 fun Context.showToast(text: CharSequence, duration: Int = Toast.LENGTH_LONG) = Toast.makeText(this, text, duration).show()
 
@@ -12,3 +17,5 @@ fun Activity.launchActivity(destinationClass: Class<*>, flags: Int = 0) {
   intent.flags = flags
   startActivity(intent)
 }
+
+fun ViewGroup.inflate(layoutRes: Int): View = LayoutInflater.from(context).inflate(layoutRes, this, false)
--- Umbrella/src/main/java/com/foo/umbrella/ui/MainActivity.kt
+++ Umbrella/src/main/java/com/foo/umbrella/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.preference.PreferenceManager
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
 import com.foo.umbrella.R
 import com.foo.umbrella.data.ApiServicesProvider
 import com.foo.umbrella.data.model.WeatherData
@@ -11,9 +12,12 @@ import com.foo.umbrella.launchActivity
 import com.foo.umbrella.preseter.MainPresenter
 import com.foo.umbrella.showToast
 import kotlinx.android.synthetic.main.activity_main.city
+import kotlinx.android.synthetic.main.activity_main.description
 import kotlinx.android.synthetic.main.activity_main.header
 import kotlinx.android.synthetic.main.activity_main.settings
 import kotlinx.android.synthetic.main.activity_main.temperature
+import kotlinx.android.synthetic.main.day_forecast.hours
+import org.threeten.bp.LocalDateTime
 
 class MainActivity: AppCompatActivity() {
 
@@ -32,14 +36,27 @@ class MainActivity: AppCompatActivity() {
     val zip = preferences.getString(getString(R.string.settings_zip_key), getString(R.string.default_zipcode))
     val unit = preferences.getString(getString(R.string.settings_units_key), getString(R.string.unit_celsius))
     presenter.fetchData(zip, { updateUi(it, unit) }, this::showMessage)
+
+    val numberOfColumns = 4
+
+    hours.layoutManager = GridLayoutManager(this, numberOfColumns)
+    hours.adapter = HourlyForecastAdapter()
   }
 
   private fun updateUi(weatherData: WeatherData, unit: String) {
+    val isCelsius = unit.equals(getString(R.string.unit_celsius))
     with(weatherData.currentObservation) {
-      val tempUnit = if (unit.equals(getString(R.string.unit_celsius))) tempCelsius else tempFahrenheit
+      val tempUnit = if (isCelsius) tempCelsius else tempFahrenheit
       temperature.text = getString(R.string.temperature, tempUnit)
       city.text = displayLocation.fullName
+      description.text = weatherDescription
       header.setBackgroundColor(ContextCompat.getColor(this@MainActivity, presenter.getTemperatureColor(tempFahrenheit)))
+    }
+
+    with(hours.adapter as HourlyForecastAdapter) {
+      forecast = weatherData.forecast.filter { it.dateTime.dayOfYear.equals(LocalDateTime.now().dayOfYear) }
+      showCelsius = isCelsius
+      notifyDataSetChanged()
     }
   }
 
